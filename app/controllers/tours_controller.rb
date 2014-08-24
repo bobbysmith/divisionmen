1c1718709a92ad70c63b36315b163c800ac0ea3c
Bobby Smith <bobbysmith24@gmail.com>
2014-08-24 18:45:53 -0500

    blog controller, model, views

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,5 +1,5 @@
 class ToursController < ApplicationController
-  # before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:index, :show]
 
   def index
     @tours = Tour.all
