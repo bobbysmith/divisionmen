23dc114b87a6c6a3271eeb7e7fc172b78ba2c11c
Bobby Smith <bobbysmith24@gmail.com>
2014-08-24 16:21:54 -0500

    tour controller methods

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,3 +1,51 @@
 class ToursController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+    @tours = Tour.all
+  end
+
+  def show
+    @tour = Tour.find(params[:id])
+  end
+
+  def new
+    @tour = Tour.new
+  end
+
+  def create
+    tour = Tour.new(tour_params)
+    tour.save
+    redirect_to tours_path
+  end
+
+  def edit
+    @tour = Tour.find(params[:id])
+  end
+
+  def update
+    @tour = Tour.find(params[:id])
+
+    %w{venue url city date}.each do |param_name|
+      if params[:tour][param_name].blank?
+        params[:tour].delete(param_name)
+      end
+    end
+
+    @tour.update(tour_params)
+    redirect_to tours_path
+  end
+
+  def destroy
+    @tour = Tour.find(params[:id])
+    @tour.delete
+    redirect_to tours_path
+  end
+
+  private
+
+  def tour_params
+    params.require(:tour).permit(:venue, :city, :url, :date)
+  end
 
 end
