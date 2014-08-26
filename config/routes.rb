1a3ecc64959a4f057dea7728eb812721d08bba2a
Bobby Smith <bobbysmith24@gmail.com>
2014-08-26 16:33:33 -0500

    hover effects, clean up divs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "home#index"
   devise_for :users
+  devise_scope :user do
+    get "/login" => "devise/sessions#new"
+  end
   resources :tours
   resources :blogs
   resources :videos
