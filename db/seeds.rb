b95b1ab4d43816537e1355dc8b0c97f4231177d3
Bobby Smith <bobbysmith24@gmail.com>
2014-12-10 15:24:21 -0600

    add seed for photo

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-users = User.create([{email: "imlostinberlin@yahoo.com", encrypted_password: "password1"}])
+# users = User.create([{email: "imlostinberlin@yahoo.com", encrypted_password: "password1"}])
 
-videos = Video.create([{title: "Always", url: "akiJdM2X_-Q"}])
+# videos = Video.create([{title: "Always", url: "akiJdM2X_-Q"}])
 
-songs = Song.create([{title: "We Can Dream", url: "https://soundcloud.com/divisionmen/we-can-dream"}])
+# songs = Song.create([{title: "We Can Dream", url: "https://soundcloud.com/divisionmen/we-can-dream"}])
 
-tours = Tour.create([{venue: "Mohawk", city: "Austin, TX", url: "http://mohawkaustin.com", date: "August, 25 2014", bands: "Stevie Wonder"}])
+# tours = Tour.create([{venue: "Mohawk", city: "Austin, TX", url: "http://mohawkaustin.com", date: "August, 25 2014", bands: "Stevie Wonder"}])
 
-blogs = Blog.create([{title: "TV", article: "We have a new video up on youtube. We have also got a new album available for purchase. It's called 'Under the Gun' We have got some shows listed as well. Thnx. DVM"}])
+# blogs = Blog.create([{title: "TV", article: "We have a new video up on youtube. We have also got a new album available for purchase. It's called 'Under the Gun' We have got some shows listed as well. Thnx. DVM"}])
+
+photo = Photo.create([{url: "https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xaf1/v/t1.0-9/10801743_10152551929483963_5559661729038456052_n.jpg?oh=7a64e34563c6efdfb355335a23a512eb&oe=550F883D&__gda__=1426449545_d2b82459c3b54bf9f6602506c5258135"}])
