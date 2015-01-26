cc0b054b847f292a320455c2aa37aebb8a40696e
Bobby Smith <bobbysmith24@gmail.com>
2015-01-25 22:22:05 -0600

    ordering

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,8 +1,8 @@
 class AboutController < ApplicationController
 
   def index
-    @photos = Photo.order('created_at ASC')
-    @reviews = Review.order('created_at DESC')
+    @photos = Photo.all
+    @reviews = Review.all 
   end
 
 end
