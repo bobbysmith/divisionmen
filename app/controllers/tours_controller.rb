83015d3f8f39291b5061dbc8161856644c613115
Bobby Smith <bobbysmith24@gmail.com>
2016-02-12 12:12:43 -0600

    descending shows

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -2,7 +2,7 @@ class ToursController < ApplicationController
   before_filter :authenticate_user!, :except => [:index, :show]
 
   def index
-    @tours = Tour.order('date ASC')
+    @tours = Tour.order('date DESC')
   end
 
   def show
